1ca31a16c8666ebafc1b2020783f59a0d07b97d0
Natalie Fioretti <natalie.fioretti@postfinance.ch>
2018-10-15 17:43:32 +0200

    fixed comment

--- pkg/fio/fio.go
+++ pkg/fio/fio.go
@@ -4,7 +4,7 @@ package fio
 //var fiomaps []*FIOMap // oder map[string]FIOMap
 var fiomaps map[string]*FIOMap = make(map[string]*FIOMap) // oder map[string]FIOMap
 
-// RegisterProvder registers FIOMaps
+// RegisterProvider registers FIOMaps
 func RegisterProvider(fm *FIOMap) {
 	fiomaps[fm.MountPath] = fm
 	//fiomaps = append(fiomaps, fm)
