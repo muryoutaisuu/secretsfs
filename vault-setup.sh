b8de2b2f3d77b064e7e548a2ee9540ffd7763021
Natalie Fioretti <natalie.fioretti@postfinance.ch>
2018-10-21 18:56:22 +0200

    fixed vault authentication, but cant yet list -> what struct is that interface thingy??

--- vault-setup.sh
+++ vault-setup.sh
@@ -1,9 +1,10 @@
 vault server -dev &
 export VAULT_ADDR='http://127.0.0.1:8200'
 sleep 1
-echo -n "type in root roleid: "
+echo -n "type in root token: "
 read ROOT
 export VAULT_TOKEN="$ROOT"
+echo "export ROOTTOKEN=$VAULT_TOKEN" > sourceit
 vault kv put secret/hello foo=world
 vault auth enable approle
 vault policy write mury vault-policy-mury.txt
@@ -11,7 +12,14 @@ vault write auth/approle/role/root policies=default,mury bind_secret_id=false to
 vault read auth/approle/role/root/role-id
 echo -n "type in roleid: "
 read ROLEID
-vault write auth/approle/login role_id=$ROLEID
+export ROLEID
 echo "$ROLEID" > /root/.vault-roleid
+echo "export ROLEID=$ROLEID" >> sourceit
+vault write auth/approle/login role_id=$ROLEID
+echo -n "type in root approle token:"
+read ROLETOKEN
+export ROLETOKEN
+echo "export ROLETOKEN=$ROLETOKEN" >> sourceit
+echo "export VAULT_TOKEN=$ROLETOKEN" >> sourceit
 vault kv list secret
 vault kv get secret/hello
