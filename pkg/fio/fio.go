0e0aa6de3957e697d8042948726faf31ff16ccc8
Natalie Fioretti <natalie.fioretti@postfinance.ch>
2018-12-06 10:46:24 +0100

    documented fio.go

--- pkg/fio/fio.go
+++ pkg/fio/fio.go
@@ -1,3 +1,13 @@
+// FUSE Input/Output (FIO)
+//
+// FIO stands for 'FUSE Input/Output' and provides the interface for programming
+// FIO plugins for secretsfs. Those FIO plugins need to be registered with the
+// fio.RegisterProvider(fm FIOMap) function.
+// The FIOMap makes sure that mountpath in the high-top filesystem and FIO plugin
+// are always mapped correctly.
+// 
+// Also initializes variable Log for shared (and consistent) Logging.
+
 package fio
 
 import(
@@ -6,7 +16,7 @@ import(
 )
 
 // fiomaps contains all FIOMaps, that map FIOProvider to MountPaths
-//var fiomaps []*FIOMap // oder map[string]FIOMap
+// var fiomaps []*FIOMap // oder map[string]FIOMap
 var fiomaps map[string]*FIOMap = make(map[string]*FIOMap) // oder map[string]FIOMap
 
 // Log contains all the needed Loggers
@@ -15,12 +25,16 @@ var Log *sfslog.Log = sfslog.Logger()
 // sto contains the currently set store
 var sto store.Store
 
-// RegisterProvider registers FIOMaps
+// RegisterProvider registers FIOMaps.
+// To be used inside of init() Function of Plugins
 func RegisterProvider(fm *FIOMap) {
 	fiomaps[fm.MountPath] = fm
 	//fiomaps = append(fiomaps, fm)
 }
 
+// FIOMaps returns all registered FIO Plugins mapped with their mountpaths.
+// Generally used by high-top secretsfs filesystem so it can correctly redirect
+// calls to corresponding plugins.
 func FIOMaps() map[string]*FIOMap {
 	return fiomaps
 }
