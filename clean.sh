af749af423cb500a424d10c37cb57cff4e5f97e9
Natalie Fioretti <naadl.93+github@gmail.com>
2019-01-23 21:06:17 +0100

    strange behaviour without sleep 1 in clean.sh

--- clean.sh
+++ clean.sh
@@ -2,4 +2,5 @@ export SECRETSFS_FILE_ROLEID=".vault-roleid"
 export VAULT_ADDR='http://127.0.0.1:8200'
 umount /mnt/secretsfs
 go build ./cmd/secretsfs
+sleep 1
 ./secretsfs /mnt/secretsfs -o allow_other
