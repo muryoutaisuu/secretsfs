970e60f1d5309fe5afa99e8177cf1c4f3963ced2
Natalie Fioretti <natalie.fioretti@postfinance.ch>
2019-06-13 15:28:00 +0200

    gofmt

--- pkg/fio/secretsfiles.go
+++ pkg/fio/secretsfiles.go
@@ -8,7 +8,7 @@ import (
 // FIOSecretsfiles is a Filesystem implementing the FIOPlugin interface that
 // outputs secrets directly when doing a command like:
 //	cat <mountpath>/secretsfiles/<secretsItem>
-type FIOSecretsfiles struct {}
+type FIOSecretsfiles struct{}
 
 func (t *FIOSecretsfiles) GetAttr(name string, context *fuse.Context) (*fuse.Attr, fuse.Status) {
 	return sto.GetAttr(name, context)
@@ -21,7 +21,7 @@ func (t *FIOSecretsfiles) OpenDir(name string, context *fuse.Context) ([]fuse.Di
 func (t *FIOSecretsfiles) Open(name string, flags uint32, context *fuse.Context) (nodefs.File, fuse.Status) {
 	content, status := sto.Open(name, flags, context)
 	if status == fuse.OK && content != "" {
-		return  nodefs.NewDataFile([]byte(content)), status
+		return nodefs.NewDataFile([]byte(content)), status
 	}
 	return nil, status
 }
@@ -29,8 +29,6 @@ func (t *FIOSecretsfiles) Open(name string, flags uint32, context *fuse.Context)
 func (t *FIOSecretsfiles) FIOPath() string {
 	return "secretsfiles"
 }
-
-
 
 func init() {
 	fioprov := FIOSecretsfiles{}
